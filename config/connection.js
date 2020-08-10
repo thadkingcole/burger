a1e8c10d8fc7d46e352653c214e05a1e114cfc0c
Thad Cole <thadjcole@gmail.com>
2020-08-10 13:49:57 -0400

    config setup
    add dotenv for using environment variables in node

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+// required modules
+require("dotenv").config(); // use environment variables
+const mysql = require("mysql");
+
+// Set up MySQL connection.
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "burgers_db",
+});
+
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,18 @@
+// * required modules
+const connection = require("./connection");
+
+// * orm object creation
+const orm = {
+  selectAll: function () {
+    // TODO: develop selectAll() function
+  },
+  insertOne: function () {
+    // TODO: develop insertOne() function
+  },
+  updateOne: function () {
+    // TODO: develop updateOne() function
+  },
+};
+
+// * export orm
+module.exports = orm;
