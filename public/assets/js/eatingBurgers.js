7d6d4cbba4b9b5650eb97648749691206a9a8011
Thad Cole <thadjcole@gmail.com>
2020-08-13 13:49:39 -0400

    able to add burgers through client

--- public/assets/js/eatingBurgers.js
+++ public/assets/js/eatingBurgers.js
@@ -1,1 +1,22 @@
-// TODO: develop front end JS code
+// TODO: add button listeners to all buttons
+// [ ] submit button with input form
+// [ ] dynamically listen to "devour it!" buttons
+
+// * helper functions
+function addNewBurger(event) {
+  const newBurger = $("#burger").val().trim(); // get text from input-no whitespace
+  // add new burger to db & reload (default for submit button)
+  $.ajax({
+    url: "/api/burgers",
+    type: "POST",
+    data: { burger_name: newBurger },
+  });
+}
+
+// function eatBurger(event) {
+
+// }
+
+// * event listeners
+$("#new-burger").click(addNewBurger); // new burger form submition
+// $(".devour-it").click(eatBurger); // devour it buttons
